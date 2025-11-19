96696d43470003219316caaf4bf5f94faf664e94
dfinazhin <dfinazhin@gmail.com>
2025-11-20 01:22:43 +0500

    Add base page by EJS

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ import express from "express";
 const port = 3000;
 const app = express();
 
+app.set("view engine", "ejs");
+app.set("views", "pages");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
 app.listen(port, () => {
   console.log(`Server has been started on port ${port}...`);
 });
