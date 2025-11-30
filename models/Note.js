b020c77b832cf65a3ea9d01b98817a6a341b7077
dfinazhin <dfinazhin@gmail.com>
2025-11-30 16:52:55 +0500

    Configuring models, add validator

--- models/Note.js
+++ models/Note.js
@@ -1,18 +1,49 @@
 import mongoose from "mongoose";
+//import moduleName from "validator";
 
-const NoteSchema = mongoose.Schema({
-  full_name: {
-    type: String,
-    required: true,
+const NoteSchema = mongoose.Schema(
+  {
+    full_name: {
+      type: String,
+      required: [true, "ФИО обязательно для заполнения"],
+      validate: {
+        validator: function (v) {
+          return /^[А-ЯЁа-яё]+(-[А-ЯЁа-яё]+)*\s+[А-ЯЁа-яё]+\s*([А-ЯЁа-яё]+)?$/.test(
+            v.trim()
+          );
+        },
+        message: "Укажите Фамилию, Имя и Отчество(при наличии).",
+      },
+    },
+    phone: {
+      type: String,
+      required: [true, "Номер телефона обязателен для заполнения"],
+      validate: {
+        validator: function (value) {
+          // Проверяем, что строка состоит только из цифр
+          return (
+            /^\d+$/.test(value) &&
+            value.length === 11 &&
+            value >= "80000000000" &&
+            value <= "89999999999"
+          );
+        },
+        message:
+          "Требуется указать полностью 11-значный номер телефона в формате 8XXXXXXXXXX",
+      },
+    },
+    description: {
+      type: String,
+      required: [true, "Описание проблемы обязателльно для заполнения"],
+      validate: {
+        validator: function (value) {
+          return value.length < 600;
+        },
+        message: "Максимальная длина описания 600 символов",
+      },
+    },
   },
-  phone: {
-    type: Number,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 export const Note = mongoose.model("Note", NoteSchema);
